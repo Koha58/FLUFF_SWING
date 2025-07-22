5d3839dcd2d274f69f4c241a55d0cd03fdb96b58
STUDENT\2312029 <hananeko2@icloud.com>
2025-07-22 14:09:40 +0900

    ステージ選択画面を作成 各ステージ画像、鍵の解除など順次追加予定

---
+++ Assets/Texture/Item/Select/SceneSelectScript.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void SelectStage(String StageName)
+    {
+        SceneManager.LoadScene(StageName);
+    }
+
+    // 鍵がついているステージは選択不可にする
+    // クリア時にクリア判定用の変数に加算していく
+    // →数に応じて鍵を解除、選択可能にする
+}
