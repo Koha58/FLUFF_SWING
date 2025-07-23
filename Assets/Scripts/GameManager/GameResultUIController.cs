fd8b40b72be067145b3335764d26541264cb432b
STUDENT\2312029 <hananeko2@icloud.com>
2025-07-23 12:25:16 +0900

    【上げ方間違えたので修正】 ビルド用のサブタイトルシーンを作成 遷移のためにGameResultUIControllerとPauseMenuUIControllerの遷移先シーン名を変更

--- Assets/Scripts/GameManager/GameResultUIController.cs
+++ Assets/Scripts/GameManager/GameResultUIController.cs
@@ -42,8 +42,10 @@ public class GameResultUIController : MonoBehaviour
 
     #region Constants
 
+    /// <summary>セレクトシーンの名前（SceneManagerで使用）</summary>
+    private const string SelectSceneName = "SelectScene";
     /// <summary>タイトルシーンの名前（SceneManagerで使用）</summary>
-    private const string TitleSceneName = "TitleScene";
+    private const string TitleSceneName = "SubTitle";
 
     /// <summary>通常のゲーム進行速度（Time.timeScale = 1）</summary>
     private const float NormalTimeScale = 1.0f;
@@ -143,7 +145,7 @@ public class GameResultUIController : MonoBehaviour
     public void ClickQuitToStageSelect()
     {
         ResumeGameTime();
-        SceneManager.LoadScene(TitleSceneName);
+        SceneManager.LoadScene(SelectSceneName);
     }
 
     #endregion
